1b30801c3fb0cb214048c22bb6ecc352720ab1fa
Kurva VijayKumar <151424728+vijay123kurva@users.noreply.github.com>
2026-08-06 22:03:12 +0530

    Time: 16 ms (56.49%) | Memory: 47.2 MB (46.98%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,55 +1,33 @@
 class Solution {
     public String minWindow(String s, String t) {
-
-        int n = s.length();
-        int left = 0;
-        int right = 0;
-        int start = -1;
-        int minLen = Integer.MAX_VALUE;
-        int count = 0;
-
-        HashMap<Character,Integer> map = new HashMap<>();
-
+        int r = 0,l = 0,start = -1,minLen = Integer.MAX_VALUE,count = 0;
+        HashMap<Character,Integer> mp = new HashMap<>();
         for(int i=0;i<t.length();i++){
             char ch = t.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            mp.put(ch,mp.getOrDefault(ch,0)+1);
         }
-
-        while(right < n){
-
-            char ch = s.charAt(right);
-
-            if(map.containsKey(ch)){
-
-                if(map.get(ch) > 0)
+        while(r<s.length()){
+            char ch1 = s.charAt(r);
+            if(mp.containsKey(ch1)){
+                if(mp.get(ch1)>0){
                     count++;
-
-                map.put(ch,map.get(ch)-1);
-            }
-
-
-            while(count == t.length()){
-
-                if(right-left+1 < minLen){
-                    minLen = right-left+1;
-                    start = left;
                 }
-
-                char cha = s.charAt(left);
-
-                if(map.containsKey(cha)){
-                    map.put(cha,map.get(cha)+1);
-
-                    if(map.get(cha) > 0)
-                        count--;
-                }
-
-                left++;
+                mp.put(ch1,mp.get(ch1)-1);
             }
-
-            right++;
+            while(count==t.length()){
+                if(r-l+1<minLen){
+                    minLen = r-l+1;
+                    start = l;
+                }
+                char ch2 = s.charAt(l);
+                if(mp.containsKey(ch2)){
+                    mp.put(ch2,mp.get(ch2)+1);
+                    if(mp.get(ch2)>0) count--;
+                }
+                l++;
+            }
+            r++;
         }
-
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start,start+minLen);
+        return minLen == Integer.MAX_VALUE?"":s.substring(start,start+minLen);
     }
 }
