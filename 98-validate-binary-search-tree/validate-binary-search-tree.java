c5dd12b2449b28410e30e30f943d4e59255a4f83
Kurva VijayKumar <151424728+vijay123kurva@users.noreply.github.com>
2026-08-19 18:14:12 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (66.51%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    public boolean helper(TreeNode root,TreeNode min,TreeNode max) {
+        if(root == null) return true;
+        if(min != null && root.val<=min.val) return false;
+        if(max != null && root.val>=max.val) return false;
+        return helper(root.left,min,root) && helper(root.right,root,max);
+        
+    }
+    public boolean isValidBST(TreeNode root) {
+        return helper(root,null,null);
+        
+    }
+}
