89c2e1c2fef6af3cf6644f9c8606b85c2cdaef7a
Kurva VijayKumar <151424728+vijay123kurva@users.noreply.github.com>
2026-08-25 13:15:01 +0530

    Time: 23 ms (97.14%) | Memory: 44.3 MB (59.29%) - LeetSync

---
+++ 133-clone-graph/clone-graph.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    private Map<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+       
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        
+        Node cloneNode = new Node(node.val, new ArrayList<>());
+        visited.put(node, cloneNode);
+
+        
+        for (Node neighbor : node.neighbors) {
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return cloneNode;
+    }
+}
