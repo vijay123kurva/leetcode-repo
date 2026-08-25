4563ffbabb1524359d90ce9a95911cad22f89f19
Kurva VijayKumar <151424728+vijay123kurva@users.noreply.github.com>
2026-08-25 13:11:18 +0530

    Time: 1 ms (11.96%) | Memory: 43.1 MB (52.52%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -0,0 +1,27 @@
+
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        if (root == null) return result;
+
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+           
+            result.addFirst(curr.val);
+
+            
+            if (curr.left != null) {
+                stack.push(curr.left);
+            }
+            if (curr.right != null) {
+                stack.push(curr.right);
+            }
+        }
+
+        return result;
+    }
+}
