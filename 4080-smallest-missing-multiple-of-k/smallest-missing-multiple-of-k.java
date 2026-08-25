246538678023b741b55483a53043e5041be46dcc
Kurva VijayKumar <151424728+vijay123kurva@users.noreply.github.com>
2026-08-25 10:12:14 +0530

    Time: 2 ms (68.20%) | Memory: 45.5 MB (38.11%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+      
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        
+        
+        int multiple = k;
+        while (true) {
+            if (!set.contains(multiple)) {
+                return multiple;
+            }
+            multiple += k;
+        }
+    }
+}
