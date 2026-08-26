ff5e46de542511412a908f7756eacaec019c18a9
Kurva VijayKumar <151424728+vijay123kurva@users.noreply.github.com>
2026-08-26 21:04:57 +0530

    Time: 1 ms (99.53%) | Memory: 42.8 MB (57.72%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.java
@@ -0,0 +1,31 @@
+
+
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> numbers = new ArrayList<>();
+        int[] factorial = new int[n];
+        
+        factorial[0] = 1;
+        for (int i = 1; i < n; i++) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            numbers.add(i);
+        }
+
+       
+        k--;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = n - 1; i >= 0; i--) {
+            int index = k / factorial[i];
+            sb.append(numbers.get(index));
+            numbers.remove(index);
+            k %= factorial[i];
+        }
+
+        return sb.toString();
+    }
+}
